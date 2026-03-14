35df177b78b2930b262d7dffdffb18669946e14f
Gennadiy Tsekhmistro <Tsekhmistro.gennadiy@gmail.com>
2026-03-14 20:11:13 +0200

    modified:   js/task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -14,13 +14,16 @@ class Storage {
   }
 
   removeItem(itemToRemove) {
-    this.#items = this.#items.filter(item => item !== itemToRemove);
+    const index = this.#items.indexOf(itemToRemove);
+    if (index !== -1) {
+      this.#items.splice(index, 1);
+    }
   }
 }
 
-const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
-storage.addItem("Droid");
+storage.addItem('Droid');
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-storage.removeItem("Prolonger");
+storage.removeItem('Prolonger');
 console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
